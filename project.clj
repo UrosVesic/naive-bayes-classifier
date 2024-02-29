0e86944e8f9684584c6afd7904f5520d88e28032
Uros Vesic <uros.vesic@levi9.com>
2024-02-29 17:50:26 +0100

    Adding an endpoint to classify the mail

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.csv "1.0.0"]]
+                 [org.clojure/data.csv "1.0.0"]
+                 [ring "1.9.0"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [metosin/reitit "0.7.0-alpha7"]
+                 [metosin/muuntaja "0.6.8"]]
   :main ^:skip-aot bayes.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/bayes/core.clj
+++ src/bayes/core.clj
@@ -2,7 +2,32 @@
   (:gen-class)
   (:require [clojure.data.csv :as csv]
             [bayes.classifier :as nb]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as ring-jetty]
+            [reitit.ring :as ring]
+            [muuntaja.core :as m]
+            [reitit.ring.middleware.muuntaja :as muuntaja]))
+
+(def classifier-state (atom nil))
+
+(defn classify-email [{:keys [body-params]}]
+  (let [email (:email body-params)
+        prediction (when email (nb/predict email @classifier-state))]
+    (if prediction
+      {:body {:prediction prediction}}
+      {:status 400
+       :body {:error "Email text is missing"}})))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    ["/classify" {:post classify-email}]
+    {:data {:muuntaja m/instance
+            :middleware [muuntaja/format-middleware]}})))
+
+(defn start []
+  (ring-jetty/run-jetty app {:port 3000
+                             :join? false}))
 
 (defrecord CsvRow [id label text labelNum])
 
@@ -13,7 +38,7 @@
       (doall processed-rows))))
 
 (defn -main [& args]
-  (let [csv-rows (read-csv "C:\\Users\\uros.vesic\\IdeaProjects\\clojure\\bayes-clojure\\dataset.csv")
+  (let [csv-rows (read-csv "")
         shuffled-rows (shuffle csv-rows)
         emails (map :text shuffled-rows)
         labels (map :label shuffled-rows)
@@ -22,21 +47,25 @@
         test-emails (drop 4000 emails)
         test-labels (drop 4000 labels)]
 
-    ;; Train the classifier
-    (let [classifier-state (nb/train training-emails training-labels)]
-      (let [{:keys [spam-emails ham-emails  vocabulary]} classifier-state]
-        (println "Number of spam emails:" spam-emails)
-        (println "Number of ham emails:" ham-emails)
-        (println "Vocabulary size:" (count vocabulary)))
+    ;; Train the classifier and update the global classifier state atom
+    (reset! classifier-state (nb/train training-emails training-labels))
 
-      (when (seq test-emails)
-        (let [results (map (fn [email label]
-                             (let [prediction (nb/predict email classifier-state)]
-                               (= prediction label)))
-                           test-emails test-labels)
-              correct-predictions (count (filter true? results))
-              total-predictions (count test-emails)]
-          (println (str "Correctly predicted: " correct-predictions "/" total-predictions)))))))
+    (let [{:keys [spam-emails ham-emails vocabulary]} @classifier-state]
+      (println "Number of spam emails:" spam-emails)
+      (println "Number of ham emails:" ham-emails)
+      (println "Vocabulary size:" (count vocabulary)))
+
+    (when (seq test-emails)
+      (let [results (map (fn [email label]
+                           (let [prediction (nb/predict email @classifier-state)]
+                             (= prediction label)))
+                         test-emails test-labels)
+            correct-predictions (count (filter true? results))
+            total-predictions (count test-emails)]
+        (println (str "Correctly predicted: " correct-predictions "/" total-predictions))))
+
+    ;; Start the web server
+    (start)))
 
 
 
