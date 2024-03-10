add2db01212b07e322b6776395a8ac86773aec6d
Uros Vesic <uros.vesic@levi9.com>
2024-03-10 16:18:30 +0100

    separate handlers and routes

---
+++ src/bayes/handlers.clj
@@ -0,0 +1,34 @@
+(ns bayes.handlers
+  (:require [bayes.mapper :as mapper]
+            [bayes.db :as db]
+            [bayes.classifier :as nb]
+            [bayes.preparator :as preparator]))
+
+(defn classify-email [{:keys [body-params]}]
+  (let [email (:email body-params)
+        prediction (when email (nb/predict email @preparator/classifier-state))]
+    (if prediction
+      {:body {:prediction prediction}}
+      {:status 400
+       :body {:error "Email text is missing"}})))
+
+(defn save-email-handler [{:keys [body-params]}]
+  (let [{:keys [sender receiver subject content]} body-params]
+    (if (and sender receiver subject content)
+      (do
+        (db/insert-email sender receiver subject content)
+        {:status 201})
+      {:status 400
+       :body {:error "Email data is missing"}})))
+
+(defn get-emails-handler [{:keys [path-params]}]
+  (let [receiver (:receiver path-params)
+        transformed-emails (mapper/transform-emails (db/get-received-emails receiver))
+        classified-emails (map (fn [email]
+                                 (let [email-body (str (:subject email) " " (:content email))
+                                       prediction (nb/predict email-body @preparator/classifier-state)
+                                       spam (= prediction "spam")]
+                                   (assoc email :spam spam)))
+                               transformed-emails)]
+    {:status 200
+     :body classified-emails}))
--- src/bayes/routes.clj
+++ src/bayes/routes.clj
@@ -1,48 +1,17 @@
 (ns bayes.routes
   (:require  [ring.adapter.jetty :as ring-jetty]
-             [bayes.mapper :as mapper]
-             [bayes.db :as db]
-             [bayes.classifier :as nb]
-             [bayes.preparator :as preparator]
+             [bayes.handlers :as handlers]
+
              [reitit.ring :as ring]
              [muuntaja.core :as m]
              [reitit.ring.middleware.muuntaja :as muuntaja]))
 
-(defn classify-email [{:keys [body-params]}]
-  (let [email (:email body-params)
-        prediction (when email (nb/predict email @preparator/classifier-state))]
-    (if prediction
-      {:body {:prediction prediction}}
-      {:status 400
-       :body {:error "Email text is missing"}})))
-
-(defn save-email-handler [{:keys [body-params]}]
-  (let [{:keys [sender receiver subject content]} body-params]
-    (if (and sender receiver subject content)
-      (do
-        (db/insert-email sender receiver subject content)
-        {:status 201})
-      {:status 400
-       :body {:error "Email data is missing"}})))
-
-(defn get-emails-handler [{:keys [path-params]}]
-  (let [receiver (:receiver path-params)
-        transformed-emails (mapper/transform-emails (db/get-received-emails receiver))
-        classified-emails (map (fn [email]
-                                 (let [email-body (str (:subject email) " " (:content email))
-                                       prediction (nb/predict email-body @preparator/classifier-state)
-                                       spam (= prediction "spam")]
-                                   (assoc email :spam spam)))
-                               transformed-emails)]
-    {:status 200
-     :body classified-emails}))
-
 (def app
   (ring/ring-handler
    (ring/router
-    [["/classify" {:post classify-email}]
-     ["/email/{receiver}" {:get get-emails-handler}]
-     ["/email" {:post save-email-handler}]]
+    [["/classify" {:post handlers/classify-email}]
+     ["/email/{receiver}" {:get handlers/get-emails-handler}]
+     ["/email" {:post handlers/save-email-handler}]]
     {:data {:muuntaja m/instance
             :middleware [muuntaja/format-middleware]}})))
 
